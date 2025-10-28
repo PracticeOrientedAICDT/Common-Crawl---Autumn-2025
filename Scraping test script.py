579158a744c9f58daa65bbfd316f5d85c1209f28
Tom-H-Briz <gq25877@bristol.ac.uk>
2025-10-28 16:38:32 +0000

    Add files via upload

--- Scraping test script.py
+++ Scraping test script.py
@@ -8,14 +8,17 @@ from SerphAPI import SerphSearch
 from Scrape_Utils import ScrapeToMarkdown
 from Scrape_Utils import search_and_scrape
 from Scrape_Utils import SerphSearch
+from Scrape_Utils import URL_similarity_match
+from Scrape_Utils import extract_test_case
 import os
 from google import genai
 
 s_api_key = os.environ.get('SERPER_API_KEY')
 
-SEARCH_QUERY = "HGB CONSULTING LIMITED E15 4QR company website"
+CURRENTCO = extract_test_case()  # randomly generates a test case from the CSV file
 OUTPUT_FILENAME = "search_results.csv"
 
+SEARCH_QUERY = CURRENTCO[2] + CURRENTCO [3] +CURRENTCO [0] + "company website"  # Example: using the first test case
 
 if __name__ == "__main__":
     # Ensure the API key is set
@@ -44,7 +47,7 @@ if __name__ == "__main__":
 entity_2 =all_data[0]['markdown_content']  # Example: using the first scraped page content
 print(entity_2)
 # Configure with your API key (using environment variable)
-g_api_key=os.environ["GEMINI_API_KEY"]
+g_api_key=os.environ["GOOGLE_API_KEY"]
 
 # 1. Initialize the client.
 # It automatically finds the "GEMINI_API_KEY" from your os.environ
@@ -55,17 +58,37 @@ except Exception as e:
     exit()
 
 # 2. Define your entities
-entity_1 = SEARCH_QUERY
+
 entity_2 = all_data[0]['markdown_content']  # Using the first scraped page content
 
 # 3. Create your prompt
 prompt = f"""
-You are an entity matching expert. Compare the two entities below.
-Entity 1 is extracted from companies house data, Entity 2 is extracted from web scraped data and cleaned data from web searches for the company.
-Respond with only'Yesthey are the same entity' or 'No' if they are not.
+Your task is to find a companys official website by deciding if the two 
+sets of information match.
+The following rules regarding company information needs to be 
+observed:
+1. The company name and the company name in the website 
+information must be the same, if available
+2. The company number and the company number in the website 
+information must be the same, if available
+3. The company’s address and the address in the website 
+information must be the same, if available
+4. The industry relating to the company’s SIC code and the 
+industry in the website information must be the same, if 
+available
+5. The website information should not indicate that the company is 
+based outside of the United Kingdom
+Does the information from entity 2 match the company information 
+from entity 1?
+Answer with ‘Yes’ and the corresponding URL if they match, and 
+answer with ‘No’ if they do not.
+Entity 1:
+Company name: {CURRENTCO[2]}
+Company number: {CURRENTCO[0]}
+Address post code: {CURRENTCO[3]}
+SIC codes:{CURRENTCO[4]+CURRENTCO[5]}
+Entity 2: {entity_2}
 
-Entity 1: "{entity_1}"
-Entity 2: "{entity_2}"
 Answer:
 """
 
